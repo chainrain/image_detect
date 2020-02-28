2fd7a045a5aadbae22f7fc27622cc80ff1e38af2
chairnain <44281454+chainrain@users.noreply.github.com>
2020-02-28 03:44:36 +0000

    Delete AipImageClassify.py

--- AipImageClassify.py
+++
@@ -1,7 +0,0 @@
-""" 你的 APPID AK SK """
-from aip import AipImageClassify
-
-APP_ID = '15433439'
-API_KEY = 'uL2cQxYEpGsIjvCWflNbW76d'
-SECRET_KEY = 'YTvrg6xzmnybsG5Q2Ovs58oYfpu09HrZ'
-client = AipImageClassify(APP_ID, API_KEY, SECRET_KEY)
